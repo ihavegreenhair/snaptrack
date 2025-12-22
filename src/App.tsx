b48187415d653dfd5e34e8731e141d01b7d04988
kyle peace <kylepeace.kp@gmail.com>
2025-12-22 21:27:13 +1300

    Force rebuild of App.tsx to ensure ThemeProvider inclusion

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import HomePage from './components/HomePage';
 import PartyPage from './components/PartyPage';
 import { PartyProvider } from './lib/PartyContext';
 import { ToastProvider } from './components/ui/toast';
+import { ThemeProvider } from './lib/ThemeContext';
 
 const App: React.FC = () => {
+  // Main app layout with providers
   return (
     <Router>
-      <ToastProvider>
-        <PartyProvider>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/party/:partyCode" element={<PartyPage />} />
-          </Routes>
-        </PartyProvider>
-      </ToastProvider>
+      <ThemeProvider>
+        <ToastProvider>
+          <PartyProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/party/:partyCode" element={<PartyPage />} />
+            </Routes>
+          </PartyProvider>
+        </ToastProvider>
+      </ThemeProvider>
     </Router>
   );
 };
